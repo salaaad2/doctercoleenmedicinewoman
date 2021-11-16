62aa38a03b220a41fdae2bb806f58d5119728475
salaaad2 <salaaad2@gitlab.com>
2021-11-16 14:44:01 +0100

    icanteven

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 
 int main(void) {
 int i;
-int size = 36;
+int size = 42;
 
 i = 0;
 char * arr[] = {
@@ -15,37 +15,44 @@ char * arr[] = {
 "#include <string.h>",
 "",
 "/*",
-" *hiii",
-" */",
+"*hiii",
+"*/",
 "",
 "int main(void) {",
 "int i;",
+"int size = 36;",
 "",
-"    char * arr[] = {",
-"",
-"    };",
-"",
-"    /*",
-"     *helloooo",
-"     */",
+"i = 0;",
+"char * arr[] = {",
+"\"",
+"\n",
+",",
+"};",
+"/*",
+"*helloooo",
+"*/",
 "i = 0;",
 "while (i < 10) {",
-"write(1, arr[i], 1);",
+"write(1, arr[i], strlen(arr[i]));",
+"write(1, arr[37], 1);",
 "i++;",
 "}",
-"i = 6;",
+"i = 0;",
 "while (i < size) {",
-"write(1, arr[19], 1);",
+"write(1, arr[36], 1);",
 "write(1, arr[i], strlen(arr[i]));",
-"write(1, arr[19], 1);",
+"write(1, arr[36], 1);",
+"write(1, arr[38], 1);",
+"write(1, arr[37], 1);",
 "i++;",
 "}",
 "i = 17;",
 "while (i < size) {",
-"write(1, arr[i], 1);",
+"write(1, arr[i], strlen(arr[i]));",
+"write(1, arr[37], 1);",
 "i++;",
 "}",
-"    return 0;",
+"return 0;",
 "}",
 "\"",
 "\n",
@@ -55,24 +62,24 @@ char * arr[] = {
 *helloooo
 */
 i = 0;
-while (i < 10) {
+while (i < 12) {
 write(1, arr[i], strlen(arr[i]));
-write(1, arr[37], 1);
+write(1, arr[44], 1);
 i++;
 }
 i = 0;
 while (i < size) {
-write(1, arr[36], 1);
+write(1, arr[43], 1);
 write(1, arr[i], strlen(arr[i]));
-write(1, arr[36], 1);
-write(1, arr[38], 1);
-write(1, arr[37], 1);
+write(1, arr[43], 1);
+write(1, arr[45], 1);
+write(1, arr[44], 1);
 i++;
 }
 i = 17;
 while (i < size) {
 write(1, arr[i], strlen(arr[i]));
-write(1, arr[37], 1);
+write(1, arr[44], 1);
 i++;
 }
 return 0;
