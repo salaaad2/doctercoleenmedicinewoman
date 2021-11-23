f58f00867ceb34e759c1c93483a21ad1fe85d28f
salaaad2 <arthurdurant263@gmail.com>
2021-11-23 16:17:35 +0100

    sully locked and loaded

--- sully/c/src/cool.c
+++ sully/c/src/cool.c
@@ -4,7 +4,7 @@
 #include <stdlib.h> 
 #define NEXT "Sully_%d.c"
 #define TEXT "#include <fcntl.h> %c#include <unistd.h> %c#include <stdio.h> %c#include <stdlib.h> %c\
-#define NEXT %cSully_\%d.c%c%c \
+#define NEXT %cSully_%cd.c%c%c \
 #define TEXT %c%s%c%c                           \
  int main(void) { \
 char name[96]; \
@@ -26,7 +26,7 @@ int main(void) {     char name[96];
     int n = 5;
     sprintf(name, NEXT, (n - 1));
     fd = open(name, O_RDWR | O_CREAT, 0644);
-    dprintf(fd, TEXT, 10, 10, 10, 10, 34, (n - 1), 34, 10, 34, TEXT,34, 10, (n - 1), 34, 37, 37, 37, 34, 10);
+    dprintf(fd, TEXT, 10, 10, 10, 10, 34, 37, (n - 1), 34, 10, 34, TEXT,34, 10, (n - 1), 34, 37, 37, 37, 34, 10);
     close(fd);
     sprintf(buf, "clang ./%s -o %.7s ; ./%.7s", name, name,name);
     system(buf);
