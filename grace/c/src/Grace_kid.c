ce50e818a780ed98d866a749e520504d96cbfd1c
salaaad2 <arthurdurant263@gmail.com>
2021-11-23 10:32:33 +0100

    grace is cool

--- grace/c/src/Grace_kid.c
+++
@@ -1,7 +0,0 @@
-#include <fcntl.h>
- #include <unistd.h>
- #include <string.h>
- /*cool*/
- # define COOLKID "Grace_kid.c" # define TEXT "#include <fcntl.h>%c #include <unistd.h>%c #include <string.h>%c /*cool*/%c # define COOLKID %cGrace_kid.c%c # define TEXT %c%s%c%c # define GRACE() int main(void) {int fd = open(COOLKID, O_RDWR | O_CREAT, 0644); write(fd, COOLKID, strlen(COOLKID)); close(fd); return 0;}%cGRACE()"
- # define GRACE() int main(void) {int fd = open(COOLKID, O_RDWR | O_CREAT, 0644); write(fd, COOLKID, strlen(COOLKID)); close(fd); return 0;}
-GRACE()
--- grace/c/src/ident.c
+++
@@ -1,8 +0,0 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-/*cool*/
-# define COOLKID "Grace_kid.c"
-# define TEXT "#include <fcntl.h>%c #include <unistd.h>%c #include <string.h>%c /*cool*/%c # define COOLKID %cGrace_kid.c%c # define TEXT %c%s%c%c # define GRACE() int main(void) {int fd = open(COOLKID, O_RDWR | O_CREAT, 0644); write(fd, COOLKID, strlen(COOLKID)); close(fd); return 0;}%cGRACE()"
-# define GRACE() int main(void) {int fd = open(COOLKID, O_RDWR | O_CREAT, 0644); dprintf(fd, TEXT, 10,10,10,10,34,34,34,TEXT,34,10,10); close(fd); return 0;}
-GRACE()
