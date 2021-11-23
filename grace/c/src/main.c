166209011d669e434604e9f6de46892f2b1bb0d5
salaaad2 <arthurdurant263@gmail.com>
2021-11-23 10:19:55 +0100

    nice

--- grace/c/src/main.c
+++ grace/c/src/main.c
@@ -1,1 +1,5 @@
-#define PRINT(x) cool
+#define COOLKID "Grace_kid.c"
+#define SOURCE "#include <stdio.h>%cvoid hello(){}/*hello*/int main(){char*c=%c%s%c;printf(c,10,34,c,34,10);hello();/*hello*/return 0;}%c"
+#define GRACE() int main(void) {}
+
+GRACE()
