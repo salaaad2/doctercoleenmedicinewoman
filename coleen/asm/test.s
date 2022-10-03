4e1df1e4c74672dbd2dea9969b8fcf6e80e59dd4
salaaad2 <arthurdurant263@gmail.com>
2022-10-03 14:04:55 +0200

    verycool

---
+++ coleen/asm/test.s
@@ -0,0 +1,22 @@
+; cool comment
+global main
+extern printf
+
+section .rodata
+code_str db "; cool comment%1$cglobal main%1$cextern printf%1$c%1$csection .rodata%1$ccode_str db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cmain:%1$center 0, 0%1$c; new stack frame%1$clea rdi, [rel code_str]%1$cmov rsi, 10%1$cmov rdx, 34%1$clea rcx, [rel code_str]%1$ccall printf%1$cleave%1$c; leave it%1$ccall return%1$creturn:%1$cxor eax, eax%1$cret%1$c", 0
+
+section .text
+main:
+enter 0, 0
+; new stack frame
+lea rdi, [rel code_str]
+mov rsi, 10
+mov rdx, 34
+lea rcx, [rel code_str]
+call printf
+leave
+; leave it
+call return
+return:
+xor eax, eax
+ret
--- coleen/c/asm/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int
-main(void)
-{
-    printf("cool %d\n", 1);
-}
