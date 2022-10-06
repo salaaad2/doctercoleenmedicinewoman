ba7a03787a387cb028951f43c2661cf00226ed20
salaaad2 <arthurdurant263@gmail.com>
2022-10-06 11:14:44 +0200

    grace DONE

--- Grace/asm/Grace.s
+++ Grace/asm/Grace.s
@@ -3,33 +3,30 @@ global main
 extern dprintf
 extern close
 extern open
-
 %define OPEN_FLAGS 66
 %define OPEN_PERMISSIONS 420
 %define CHAR_NEWLINE 10
-
 %macro grace 0
 section .rodata
-    file_str db "./Grace_kid.s", 0
-    code_str db "symbols: %1$c %2$c %3$s", 0
+file_str db "./Grace_kid.s", 0
+code_str db "; cool comment%1$cglobal main%1$cextern dprintf%1$cextern close%1$cextern open%1$c%4$cdefine OPEN_FLAGS 66%1$c%4$cdefine OPEN_PERMISSIONS 420%1$c%4$cdefine CHAR_NEWLINE 10%1$c%4$cmacro grace 0%1$csection .rodata%1$cfile_str db %2$c./Grace_kid.s%2$c, 0%1$ccode_str db %2$c%3$s%2$c, 0%1$csection .text%1$cmain:%1$c;;  new stack frame%1$center 0,0%1$cmov rdi, file_str%1$cmov rsi, OPEN_FLAGS%1$cmov rdx, OPEN_PERMISSIONS%1$cmov rax, 0%1$ccall open%1$cmov rdi, rax%1$clea rsi, [rel code_str]%1$cmov rdx, CHAR_NEWLINE%1$cmov rcx, 34%1$clea r8, [rel code_str]%1$cmov r9, 37%1$ccall dprintf%1$cleave%1$cret%1$c%4$cendmacro%1$cinsert grace%1$c", 0
 section .text
 main:
-    ;;  new stack frame
-    enter 0,0
-    mov rdi, file_str
-    mov rsi, OPEN_FLAGS
-    mov rdx, OPEN_PERMISSIONS
-    mov rax, 0
-    call open
-    ;;  dprintf fd: hardcode to 1
-    mov rdi, rax                ; rax holds open retval
-    lea rsi, [rel code_str]
-    mov rdx, CHAR_NEWLINE
-    mov rcx, 34
-    lea r8, [rel code_str]
-    call dprintf
-    leave
-    ret
+;;  new stack frame
+enter 0,0
+mov rdi, file_str
+mov rsi, OPEN_FLAGS
+mov rdx, OPEN_PERMISSIONS
+mov rax, 0
+call open
+mov rdi, rax
+lea rsi, [rel code_str]
+mov rdx, CHAR_NEWLINE
+mov rcx, 34
+lea r8, [rel code_str]
+mov r9, 37
+call dprintf
+leave
+ret
 %endmacro
-
-    insert grace
+insert grace
--- Sully/asm/Sully.s
+++ Sully/asm/Sully.s
@@ -1,43 +1,64 @@
 ; cool comment
 global main
-extern sprintf
-extern open
-extern close
 extern dprintf
-
+extern close
+extern open
+extern system
+%define OPEN_FLAGS 66
+%define OPEN_PERMISSIONS 420
+%define CHAR_NEWLINE 10
+%define CHAR_QUOTE 34
+%define CHAR_PERCENT 37
 section .rodata
-    n db 5
-    buf db "", 0
-    format_file_str db "Sully_%1$d.s", 0
-    code_str db "symbols: %1$c %2$c %3$s", 0
+index dw 5, 0
+file_str db "./Sully_%d.s", 0
+code_str db "; cool comment \
+global main \
+extern dprintf \
+extern close \
+extern open \
+%4$cdefine OPEN_FLAGS 66 \
+%4$cdefine OPEN_PERMISSIONS 420 \
+%4$cdefine CHAR_NEWLINE 10 \
+%4$cmacro grace 0 \
+section .rodata \
+file_str db %2$c./Grace_kid.s%2$c, 0 \
+code_str db %2$c%3$s%2$c, 0 \
+section .text \
+main: \
+;;  new stack frame \
+enter 0,0 \
+mov rdi, file_str \
+mov rsi, OPEN_FLAGS \
+mov rdx, OPEN_PERMISSIONS \
+mov rax, 0 \
+call open \
+mov rdi, rax \
+lea rsi, [rel code_str] \
+mov rdx, CHAR_NEWLINE \
+mov rcx, 34 \
+lea r8, [rel code_str] \
+mov r9, 37 \
+call dprintf \
+leave \
+ret \
+%4$cendmacro \
+insert grace ", 0
 section .text
 main:
-;; new stack frame
-    cmp n, 0x0
-    je exit
-;; rm 1
-    sub n, 1
+    ;;  new stack frame
     enter 0,0
-    mov rdi, buf
-    mov rsi, format_file_str
-    mov rdx, n
-    call sprintf
-;;
-    mov rdi, target_file_str
-    mov rsi, 66
-    mov rdx, 420
+    mov rdi, file_str
+    mov rsi, OPEN_FLAGS
+    mov rdx, OPEN_PERMISSIONS
     mov rax, 0
     call open
-;;  dprintf fd: hardcode to 1
-    mov rdi, rax                ; rax holds open retval
+    mov rdi, rax
     lea rsi, [rel code_str]
-    mov rdx, 10
-    mov rcx, 34
+    mov rdx, CHAR_NEWLINE
+    mov rcx, CHAR_QUOTE
     lea r8, [rel code_str]
+    mov r9, CHAR_PERCENT
     call dprintf
     leave
-    ret
-
-exit:
-    xor rax, rax
     ret
