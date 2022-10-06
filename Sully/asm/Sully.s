57a080ca62fec93ff292b55a3331496cf7431fef
salaaad2 <arthurdurant263@gmail.com>
2022-10-06 23:17:15 +0200

    insert cheese

--- Sully/asm/Sully.s
+++ Sully/asm/Sully.s
@@ -9,9 +9,72 @@ section .bss
 buf resb 256
 section .data
 output db "verylongstrusedasoutput", 0
-command_str db "nasm -f elf64 %s ", 0
-file_str db "./Sully_%d.s", 0
-code_str db " %1$c  ret %4$d ", 0
+command_str db "nasm -f elf64 %1$s ; clang %.7s.o -o %1$.7s ; ./%1$.7s", 0
+file_str db "Sully_%d.s", 0
+code_str db " %1$c \
+; cool comment %1$c \
+global main %1$c \
+extern dprintf %1$c \
+extern sprintf %1$c \
+extern close %1$c \
+extern open %1$c \
+extern system %1$c \
+section .bss %1$c \
+buf resb 256 %1$c \
+section .data %1$c \
+output db %2$cverylongstrusedasoutput%2$c, 0 %1$c \
+command_str db %2$cnasm -f elf64 Sully_4.s ; clang Sully_4.o -o Sully_4 ; ./Sully_4%2$c, 0 %1$c \
+file_str db %2$cSully_%d.s%2$c, 0 %1$c \
+code_str db %2$c%3$s%2$c, 0 %1$c \
+index dq %4$d %1$c \
+section .text %1$c \
+main: %1$c \
+    ;;  new stack frame %1$c \
+    enter 0,0 %1$c \
+    dec qword [index] %1$c \
+    xor rax, rax %1$c \
+    lea rdi, output %1$c \
+    lea rsi, file_str %1$c \
+    mov rdx, [index] %1$c \
+    ;; sprintf(output, file_str, index) %1$c \
+    call sprintf %1$c \
+ %1$c \
+    xor rax, rax %1$c \
+    mov rdi, output %1$c \
+    mov rsi, 66 %1$c \
+    mov rdx, 420 %1$c \
+    mov rax, 0 %1$c \
+    ;; open(output, O_RDWR |[...], 0644) %1$c \
+    call open %1$c \
+ %1$c \
+    mov rdi, rax %1$c \
+    lea rsi, [rel code_str] %1$c \
+    mov rdx, 10 %1$c \
+    mov rcx, 34 %1$c \
+    lea r8, [rel code_str] %1$c \
+    mov r9, [index] %1$c \
+    call dprintf %1$c \
+ %1$c \
+    xor rax, rax %1$c \
+    cmp qword [index], 0 %1$c \
+    jne compile_and_run %1$c \
+    leave %1$c \
+    ret %1$c \
+ %1$c \
+compile_and_run: %1$c \
+    mov rdi, buf %1$c \
+    mov rsi, command_str %1$c \
+    mov rdx, output %1$c \
+    ;; sprintf(buf, command_str, output) %1$c \
+    call sprintf %1$c \
+ %1$c \
+    xor rax, rax %1$c \
+    mov rdi, buf %1$c \
+    ;; system(buf) %1$c \
+    call system %1$c \
+    xor rax, rax %1$c \
+    leave %1$c \
+    ret", 0
 index dq 5
 section .text
 main:
