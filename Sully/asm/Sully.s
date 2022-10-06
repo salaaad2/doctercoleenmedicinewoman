45040eae57f9683a2ece5bcf66c915c5a9520618
salaaad2 <arthurdurant263@gmail.com>
2022-10-06 12:48:52 +0200

    cant dec a num

--- Sully/asm/Sully.s
+++ Sully/asm/Sully.s
@@ -1,6 +1,7 @@
 ; cool comment
 global main
 extern dprintf
+extern sprintf
 extern close
 extern open
 extern system
@@ -10,49 +11,30 @@ extern system
 %define CHAR_QUOTE 34
 %define CHAR_PERCENT 37
 section .rodata
-index dw 5, 0
+buf db "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 0
+output db "verylongstrusedasoutput", 0
+command_str db "nasm -f elf64 %s", 0
+index dq 5
 file_str db "./Sully_%d.s", 0
-code_str db "; cool comment \
-global main \
-extern dprintf \
-extern close \
-extern open \
-%4$cdefine OPEN_FLAGS 66 \
-%4$cdefine OPEN_PERMISSIONS 420 \
-%4$cdefine CHAR_NEWLINE 10 \
-%4$cmacro grace 0 \
-section .rodata \
-file_str db %2$c./Grace_kid.s%2$c, 0 \
-code_str db %2$c%3$s%2$c, 0 \
-section .text \
-main: \
-;;  new stack frame \
-enter 0,0 \
-mov rdi, file_str \
-mov rsi, OPEN_FLAGS \
-mov rdx, OPEN_PERMISSIONS \
-mov rax, 0 \
-call open \
-mov rdi, rax \
-lea rsi, [rel code_str] \
-mov rdx, CHAR_NEWLINE \
-mov rcx, 34 \
-lea r8, [rel code_str] \
-mov r9, 37 \
-call dprintf \
-leave \
-ret \
-%4$cendmacro \
-insert grace ", 0
+code_str db "; cool comment insert grace ", 0
 section .text
 main:
     ;;  new stack frame
     enter 0,0
-    mov rdi, file_str
+    dec qword [index]
+    mov rdi, output
+    mov rsi, file_str
+    mov rdx, index
+    xor rax, rax
+    call sprintf
+
+    xor rax, rax
+    mov rdi, output
     mov rsi, OPEN_FLAGS
     mov rdx, OPEN_PERMISSIONS
     mov rax, 0
     call open
+
     mov rdi, rax
     lea rsi, [rel code_str]
     mov rdx, CHAR_NEWLINE
@@ -60,5 +42,21 @@ main:
     lea r8, [rel code_str]
     mov r9, CHAR_PERCENT
     call dprintf
+
+    xor rax, rax
+    cmp word [index], 0
+    jne compile_and_run
+    leave
+    ret
+
+compile_and_run:
+    mov rdi, buf
+    mov rsi, command_str
+    mov rdx, output
+    call sprintf
+
+    mov rdi, rax
+    call system
+    xor rax, rax
     leave
     ret
