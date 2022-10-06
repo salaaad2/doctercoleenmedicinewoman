316049de120e16918a9784f2ab83d4a0c2ac0118
salaaad2 <arthurdurant263@gmail.com>
2022-10-06 18:32:14 +0200

    finally got ASM_Sully to work + fix C_Grace

--- Sully/asm/Sully.s
+++ Sully/asm/Sully.s
@@ -10,22 +10,25 @@ extern system
 %define CHAR_NEWLINE 10
 %define CHAR_QUOTE 34
 %define CHAR_PERCENT 37
-section .rodata
-buf db "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 0
+section .bss
+buf resb 256
+section .data
+buf2 db "echo cool", 0
 output db "verylongstrusedasoutput", 0
-command_str db "nasm -f elf64 %s", 0
-index dq 5
+command_str db "nasm -f elf64 %s; clang %.7s.o -o %.7s ; ./%.7s", 0
 file_str db "./Sully_%d.s", 0
-code_str db "; cool comment insert grace ", 0
+code_str db "; cool comment insert grace%c %c %s %c ", 0
+index dq 5
 section .text
 main:
     ;;  new stack frame
     enter 0,0
     dec qword [index]
-    mov rdi, output
-    mov rsi, file_str
-    mov rdx, index
     xor rax, rax
+    lea rdi, output
+    lea rsi, file_str
+    mov rdx, [index]
+    ;; sprintf(output, file_str, index)
     call sprintf
 
     xor rax, rax
@@ -33,6 +36,7 @@ main:
     mov rsi, OPEN_FLAGS
     mov rdx, OPEN_PERMISSIONS
     mov rax, 0
+    ;; open(output, O_RDWR | )
     call open
 
     mov rdi, rax
@@ -41,10 +45,11 @@ main:
     mov rcx, CHAR_QUOTE
     lea r8, [rel code_str]
     mov r9, CHAR_PERCENT
+    ;; dprintf(fd=3, code_str, \n, ", code_str, %)
     call dprintf
 
     xor rax, rax
-    cmp word [index], 0
+    cmp qword [index], 0
     jne compile_and_run
     leave
     ret
@@ -53,9 +58,11 @@ compile_and_run:
     mov rdi, buf
     mov rsi, command_str
     mov rdx, output
+    ;; sprintf(buf, command_str, output)
     call sprintf
 
-    mov rdi, rax
+    xor rax, rax
+    mov rdi, buf
     call system
     xor rax, rax
     leave
